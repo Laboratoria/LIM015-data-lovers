5708c7f9c74270ed47e4d985aea17956ab8bebe6
Mariacampos25 <marian.234cs@gmail.com>
2021-06-21 18:27:55 -0500

    navegacion

--- src/main.js
+++ src/main.js
@@ -3,27 +3,32 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("nav-menu_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menú");
+    }
+});
+
 document.getElementById("atletas").addEventListener("click", () => {
     document.getElementById("firstScreen").style.display = "none";
+    document.getElementById("thirdScreen").style.display = "none";
     document.getElementById("secondScreen").style.display = "block";
 });
 document.getElementById("paises").addEventListener("click", () => {
     document.getElementById("firstScreen").style.display = "none";
-    document.getElementById("thirdScreen").style.display = "block";
-});
-document.getElementById("paises2").addEventListener("click", () => {
     document.getElementById("secondScreen").style.display = "none";
     document.getElementById("thirdScreen").style.display = "block";
 });
 document.getElementById("home").addEventListener("click", () => {
     document.getElementById("secondScreen").style.display = "none";
-    document.getElementById("firstScreen").style.display = "block";
-});
-document.getElementById("atletas2").addEventListener("click", () => {
-    document.getElementById("thirdScreen").style.display = "none";
-    document.getElementById("secondScreen").style.display = "block";
-});
-document.getElementById("home2").addEventListener("click", () => {
     document.getElementById("thirdScreen").style.display = "none";
     document.getElementById("firstScreen").style.display = "block";
 });
