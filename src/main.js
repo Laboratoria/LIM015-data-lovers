aebf5c25ed938fab3e23d24a012978f03727d332
RocioSulca <rociosulca.z@gmail.com>
2021-06-21 08:55:07 -0500

    probando

--- src/main.js
+++ src/main.js
@@ -3,4 +3,30 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+document.getElementById("atletas").addEventListener("click", () => {
+    document.getElementById("firstScreen").style.display = "none";
+    document.getElementById("secondScreen").style.display = "block";
+});
+document.getElementById("paises").addEventListener("click", () => {
+    document.getElementById("firstScreen").style.display = "none";
+    document.getElementById("thirdScreen").style.display = "block";
+});
+document.getElementById("paises2").addEventListener("click", () => {
+    document.getElementById("secondScreen").style.display = "none";
+    document.getElementById("thirdScreen").style.display = "block";
+});
+document.getElementById("home").addEventListener("click", () => {
+    document.getElementById("secondScreen").style.display = "none";
+    document.getElementById("firstScreen").style.display = "block";
+});
+document.getElementById("atletas2").addEventListener("click", () => {
+    document.getElementById("thirdScreen").style.display = "none";
+    document.getElementById("secondScreen").style.display = "block";
+});
+document.getElementById("home2").addEventListener("click", () => {
+    document.getElementById("thirdScreen").style.display = "none";
+    document.getElementById("firstScreen").style.display = "block";
+});
+
+
 console.log(example, data);
