5b877918a14b5e865d37383a6f2d9d3880996a6f
AlissonCh <rocio.cruz@unmsm.edu.pe>
2021-06-21 00:15:41 -0500

    html con footer y label de filtrado funcional

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/athletes/athletes.js';
+//muestra la opción seleccionada en el select(caja de filtrado) y guarda el valor de la selección en una variable;
+const select= document.querySelector('#select');
+const options =document.querySelector('#options');
+const contentSelect = document.querySelector('#select .content-select');
+const hiddenInput = document.querySelector('#userSelection');
+
+document.querySelectorAll('#options > .option').forEach((option)=>{
+    option.addEventListener('click', (e)=>{
+        e.preventDefault();
+        contentSelect.innerHTML=e.currentTarget.querySelector('.data').innerText;
+        console.log(e.currentTarget.innerHTML);
+        select.classList.toggle('active');
+        options.classList.toggle('active');
+        hiddenInput.value = e.currentTarget.querySelector('.data').innerText;
+    });
+});
+select.addEventListener('click', ()=>{
+    select.classList.toggle('active');
+    options.classList.toggle('active');
+});
 
 console.log(example, data);
