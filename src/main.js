cf74ad1f4b30871330443858e597f72c14089e59
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-06-28 11:44:49 -0500

    borre info que no se va a utilizar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+console.log(data.films[5].title);
+
+
