fb653cd9a46065b1fd75f6758efa82cf8cd3e9ed
Elipinto23 <elipinto23@gmail.com>
2021-06-29 13:28:23 -0500

    modificacion del json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { example } from './data.js';
+//import { pokemon } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import pokemon from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(pokemon.pokemon[250].name);
+
