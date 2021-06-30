b3691d09f9638a19706a575e9d7dccbf1d9af911
irwinet <ydamianelguera@gmail.com>
2021-06-30 11:06:18 -0500

    cambios de prueba

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import obj from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+//importar imagen y nombre el pokemon
+console.log(obj.pokemon[0]);
+
+verinfopokemon(obj.pokemon[0]);
