9af3f25a6ae0aedec33fbce09d79ec887500949c
AM27TH <amelanie.trillo27@gmail.com>
2021-06-30 19:13:36 -0500

    UImprimiendo cards

--- src/main.js
+++ src/main.js
@@ -3,4 +3,36 @@
 import data from './data/ghibli/ghibli.js';
 // import data
 
-console.log(data);
+// Selectors DOM
+const filmsContainer = document.querySelector('.container__films');
+const counter = document.querySelector(".container__showing");
+
+function showDataFilms(films){
+    films.forEach(film => {
+        const container = document.createElement('section');
+        container.className = 'container__card';
+        container.innerHTML = `
+            <section class='card__title'>
+                <h1>${film.title}</h1>
+            </section>
+            <img class='card__image' src='${film.poster}' alt='${film.title}' title='${film.title}' loading='lazy'>
+        `;
+        filmsContainer.appendChild(container);
+    })
+    counter.innerHTML = `Showing ${films.length}`;
+    /* console.log(film.title);
+    console.log(film.poster);
+    film.people.map(person => {
+        console.log(person.name);
+        console.log(person.img);
+    })
+    if(film.locations.length != 0){
+        film.locations.map(location => {
+            console.log(location.name);
+        })
+    }else{
+        console.log("No tiene locación");
+    } */
+}
+
+console.log(showDataFilms(data.films));
