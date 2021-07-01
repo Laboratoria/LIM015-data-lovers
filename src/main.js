bee5f4efcaed988c0112d869aa3eb476f9e31527
Andrea Blanco <estefania_8_3@hotmail.com>
2021-07-01 01:28:48 -0500

    agregue la funcion prueba para mostrar la data principal y unirla con el escuchar evento para que se desarrolle

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import data from './data/ghibli/ghibli.js'
 
-/*data.films.forEach(function (elem) {
-    console.log(elem.poster)
-});*/
+//console.log(data.films["people"]);
 
 const boton = document.querySelector("#pelis");
-
 boton.addEventListener("click", prueba);
 
 function prueba() {
     data.films.forEach(function (elem) {
-        const titulos = `<h2>${elem.title.toUpperCase()}</h2>`, descripciones = `<h3>Description:</h3><p>${elem.description}</p>`, directores = `<h3>Directors:</h3><p>${elem.director}</p>`, productores = `<h3>Productores:</h3><p>${elem.producer}</p>`, portada = `<img href=${elem.poster}/>`, fechaLanzamiento = `<h3>Release Date:</h3><p>${elem.release_date}</p>`, puntajeAudiencia = `${elem.release_date}`;
-        document.querySelector("#name-pelis").innerHTML += titulos + descripciones + directores + productores + fechaLanzamiento + portada;
-});
+        const peliculas = document.querySelector("#name-pelis");
+        const botonPelis = document.createElement("button");
+        peliculas.appendChild(botonPelis);
+        botonPelis.innerHTML += `<div><img src="${elem.poster}"/><h2>${elem.title.toUpperCase()}</h2></div>`;
+
+        const descripcionesPelis = document.createElement("div");
+        botonPelis.appendChild(descripcionesPelis);
+        botonPelis.addEventListener("click", function () {
+            descripcionesPelis.innerHTML += `<h3>Description:</h3><p>${elem.description}</p><h3>Directors:</h3><p>${elem.director}</p><h3>Release Date:</h3><p>${elem.release_date}</p>` + `<div>${elem.people}</div>`;//mejorar
+        })
+        });
 }
