d77c2d77b615450bd8df3756b2fbbbe4861f5261
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-07-01 13:44:41 -0500

    prueba

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import data from './data/ghibli/ghibli.js'
 
 
@@ -5,13 +6,40 @@ import data from './data/ghibli/ghibli.js'
     console.log(elem.poster)
 });*/
 
+const navMovies = document.querySelector("#movies");
+navMovies.addEventListener("click", function () {
+    const hideFirstSlide = document.querySelector("#container_to_display");
+    const logo = document.querySelector("#logo");
+    hideFirstSlide.style.display = "none";
+    logo.style.display = "none";
+});
+
+
+// function prueba() {
+//     data.films.forEach(function (elem) {
+//         const titulos = `<h2> ${elem.title.toUpperCase()} </h2>`,
+//             descripciones = `<h3>Description:</h3><p>${elem.description}</p>`,
+//             directores = `<h3>Directors:</h3><p>${elem.director}</p>`,
+//             productores = `<h3>Productores:</h3><p>${elem.producer}</p>`,
+//             portada = `<img src="${elem.poster}" alt = "portada de pelicula" loading="lazy"/> `,
+//             fechaLanzamiento = `<h3> Release Date:</h3> <p>${elem.release_date}</p>`,
+//             puntajeAudiencia = `${elem.release_date} `;
+//         document.querySelector("#name-pelis").innerHTML += titulos + descripciones + directores + productores + fechaLanzamiento + portada;
+
+//     });
+// }
+
+
+function pruebaDos() {
+    data.films.forEach(function (element) {
+        const movieCover = `<img src="${element.poster}" alt = "portada de pelicula" loading="lazy"/> `,
+            moviesTitle = `<h3> ${element.title.toUpperCase()} </h3>`
+        document.querySelector("#name-pelis").innerHTML += movieCover + moviesTitle
+    });
+}
+
+
 const boton = document.querySelector("#pelis");
 
 boton.addEventListener("click", prueba);
 
-function prueba() {
-    data.films.forEach(function (elem) {
-        const titulos = `<h2>${elem.title.toUpperCase()}</h2>`, descripciones = `<h3>Description:</h3><p>${elem.description}</p>`, directores = `<h3>Directors:</h3><p>${elem.director}</p>`, productores = `<h3>Productores:</h3><p>${elem.producer}</p>`, portada = `<img href=${elem.poster}/>`, fechaLanzamiento = `<h3>Release Date:</h3><p>${elem.release_date}</p>`, puntajeAudiencia = `${elem.release_date}`;
-        document.querySelector("#name-pelis").innerHTML += titulos + descripciones + directores + productores + fechaLanzamiento + portada;
-    });
-}
