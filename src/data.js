4ba283f6c785ff0894ddaa8bfbd7005a0d8b2fa2
BitzyG <gv.itzelandrea@gmail.com>
2021-07-01 20:06:56 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,5 @@
 //function Order by to order data
 export const sortData = (data, sortBy, sortOrder) => {
   const order = (sortOrder === 'asc') ? 1 : -1;
- /* if (sortOrder === 'asc'){
-      order = 1;
-  }else{
-      order = -1;
-  }*/
   return data.sort((a, b) => (a[sortBy] > b[sortBy] ? order: (-order)));
 };
