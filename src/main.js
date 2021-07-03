080412143f9c7ebaf2bda60332bab81535d0fa67
Andrea Blanco <estefania_8_3@hotmail.com>
2021-07-03 00:24:22 -0500

    hare una extension del codigo de Alisson para avanzar en paralelo bajo sus ultimos cambios

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
+import { example } from './data.js';
+
 import data from './data/ghibli/ghibli.js'
 
-//console.log(data.films["people"]);
+console.log(example, data);
 
-const boton = document.querySelector("#pelis");
-boton.addEventListener("click", prueba);
 
-function prueba() {
-    data.films.forEach(function (elem) {
-        const peliculas = document.querySelector("#name-pelis");
-        const botonPelis = document.createElement("button");
-        peliculas.appendChild(botonPelis);
-        botonPelis.innerHTML += `<div><img src="${elem.poster}"/><h2>${elem.title.toUpperCase()}</h2></div>`;
 
-        const descripcionesPelis = document.createElement("div");
-        botonPelis.appendChild(descripcionesPelis);
-        botonPelis.addEventListener("click", function () {
-            descripcionesPelis.innerHTML += `<h3>Description:</h3><p>${elem.description}</p><h3>Directors:</h3><p>${elem.director}</p><h3>Release Date:</h3><p>${elem.release_date}</p>` + `<div>${elem.people}</div>`;//mejorar
-        })
-        });
-}
+
+
+
