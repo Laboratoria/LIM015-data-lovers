6dd7113f92e3ea2c21451f9e19e7420687781c93
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-07-04 19:32:27 -0500

    HE BORRADO TODO MAIN

--- src/data.js
+++ src/data.js
@@ -1,9 +1,32 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+
+
+
+
+
+
+
+
+export const dataMain = (gibli) => {
+ 
+
+
+
+
+
+//export const anotherExample = () => {   return 'OMG'; };
+
+
+
+//
+//filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+
+//sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+//computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
+
+//Estos nombres de funciones y de parámetros son sola//
