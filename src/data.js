2e2f0b0be0c487a24067fab22db7becdca1836fd
irwinet <ydamianelguera@gmail.com>
2021-07-04 19:25:32 -0500

    se catualizo html Dinamico

--- src/data.js
+++ src/data.js
@@ -1,39 +1,53 @@
 /*El corazón de este proyecto es la manipulación de datos a través de arreglos y objetos.
 Te recomendamos que este archivo contenga toda la funcionalidad que corresponda a obtener,
 procesar y manipular datos (tus funciones)*/
-// export function kanto() {
+export function kanto() {
 
-// }
-
-function lookinfopokemon(objPokemon) {
-    let html = '';
-    for (let i = 0; i < objPokemon.length; i++) {
-        let url = objPokemon[i].img;
-        let name = objPokemon[i].name;
-        let number = objPokemon[i].num;
-
-        // insertando los obj con html dinamico
-        html = html + '<figure class="photoPokemon">';
-        html = html + '<p class="num"">' + number + '</p>';
-        html = html + '<img src="' + url + '"/>';
-        html = html + '<figcaption>' + name + '<button id="btn_show" class="boton" >' + '<p>ver mas</p>' + '</button>' + '</figcaption>';
-        html = html + '</figure>';
-    }
-    // console.log(html);
-    return html;
 }
 
-// let objectoKanto = new kanto();
+function filterData(data, condition) {
+
+    let arrayPokemon = data.filter(pokemon => {
+        return pokemon;
+    });
+
+    return arrayPokemon;
+}
 
 
-// export let name = () => {
-//     return 'example';
-// };
+let objectoKanto = new kanto();
 
-// export const data = () => {
-//     return 'data';
-// };
 
+export let name = () => {
+    return 'example';
+};
+
+export const data = () => {
+    return 'data';
+};
+
+//exportamos las funciones
 export {
-    lookinfopokemon
+    filterData
 }
+
+
+
+
+// elementA.addEventListener("click", () => {
+
+//     showInfo();
+
+//     let showpokemons = `<div class="newpokemon">
+//     <img src=""> <p class=textContent> 
+//     <ul>
+//     <li> Altura: '+ size1 +'> </li>
+//     <li> Peso: '+ size2 +'> </li>
+//     </ul
+//     </p>
+//     </div>
+//     `;
+//     const Fragment = document.createRange().createContextualFragment(showpokemons)
+//     document.newpokemon.appendChild(Fragment);
+
+// });
