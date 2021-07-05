23b37fe52782d84b7086be82168e1b1c13da60e3
Gina <floresgina2020@gmail.com>
2021-07-04 19:28:21 -0500

    lista en pantalla y nuevo archivo de bienvenida

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+const containerAnimes = document.getElementById('container-animes');
+
+data.films.forEach((dato) => {
+    const cardAnime = document.createElement('div');
+    cardAnime.className = "container-card-anime";
+    cardAnime.innerHTML = `
+    <div>
+    <img src= '${dato.poster}' id="image-poster"></img>
+    <p id="title"> ${dato.title} </p>
+    <p id="release"> ${"(" + dato.release_date + ")"} </p>
+    <br>
+    </div>`
+    containerAnimes.appendChild(cardAnime);
+});
+
+//console.log(example, data);
