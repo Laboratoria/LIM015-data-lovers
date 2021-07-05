cf02971b576c3c4a2f5ed1d665afdb74e8665991
Andrea Blanco <estefania_8_3@hotmail.com>
2021-07-04 22:16:23 -0500

    agregamos funciones independientes para obtener las peliculas y su respectivo ordenamiento

--- src/data.js
+++ src/data.js
@@ -1,45 +1,59 @@
-import data from './data/ghibli/ghibli.js'
-//recibe
-// estas funciones son de ejemplo
-
-//envio
-
-//Filtrado seccion Peliculas con img + titulo
-export const setMoviesTitle = () => {
-  data.films.forEach((element) => {
-    const movieCover = `<img src="${element.poster}" alt = "portada de pelicula" loading="lazy"/> `,
-      moviesTitle = `<h3> ${element.title.toUpperCase()} </h3>`
-    document.querySelector("#name-pelis").innerHTML += movieCover + moviesTitle
+//extraer la imagen y el titulo dentro de un div
+export const setMoviesTitle = element => {
+  return element.map(
+    (el) => {
+      const movieCover = `<div class="movieItem"><img src="${el.poster}" alt = "portada de pelicula"/>`;
+      const moviesTitle = `<h2>${el.title.toUpperCase()}</h2></div>`;
+      return movieCover + moviesTitle;
+    }
+  )
+}
+//ordena pelis alfabeticamente de A a Z
+export const alphabeticOrder = (filmsCopy) => filmsCopy.sort((a, b) => {
+  return a.title > b.title ? 1 : a.title < b.title ? -1 : 0;
+});
+//ordena pelis alfabeticamente de Z a A
+export const alphabeticOrderLess = (filmsCopy) => filmsCopy.sort((a, b) => {
+  if (a.title < b.title) { return 1 }
+  if (a.title > b.title) { return -1 }
+  return 0;
+});
+//ordena pelis por rating score (mayor a menor)
+export const ratingScore = (filmsCopy) =>
+  filmsCopy.sort((a, b) => {
+    if (a.rt_score > b.rt_score) { return 1 }
+    if (a.rt_score < b.rt_score) { return -1 }
+    return 0;
   });
-};
-
-//filtrado sort por RT score (puntaje segun criticos)
-const filmsCopy = [...data.films]
-// console.log (filmsCopy);
-const reviewMovieScore = filmsCopy.sort((a, b) => {
-  return parseInt(b.rt_score) - parseInt(a.rt_score) // conbierto a numero entero
-
+//ordena pelis por rating score (menor a mayor)
+export const ratingScoreLess = (filmsCopy) => filmsCopy.sort((a, b) => {
+  if (a.rt_score < b.rt_score) { return 1 }
+  if (a.rt_score > b.rt_score) { return -1 }
+  return 0;
 });
-// console.log(reviewMovieScore)
+//ordena pelis por release date (mayor a menor)
+export const realeaseDateOld = filmsCopy =>
+  filmsCopy.sort((a, b) => {
+    return Number(a.release_date - b.release_date)
+  });
+//ordena pelis por release date (menor a mayor)
+export const lastestRealeaseDate = filmsCopy => filmsCopy.sort((a, b) => {
+  return parseInt(b.release_date) - parseInt(a.release_date)
+})
+//extraer la descripcion de peliculas(Alisson)
 
-// filtrado sort para orden alfabetico 
-const alphabeticOrder = filmsCopy.sort((a, b) => {
-  if (a.title > b.title) {
-    return 1
-  }
-  if (a.title < b.title) {
-    return -1
-  } return 0
-});
-//console.log(alphabeticOrder);
+//extraer el nombre e imagen de los personajes
+export const getPeople = element => {
+  return element.map(e => e.people.map(el => {
+    const peopleImage = `<div><img src="${el.img}" />`;
+    const peopleName = `<h3>${el.name.charAt(0).toUpperCase() + el.name.slice(1)}</h3></div>`;
+    return peopleImage + peopleName;
+  }));
+}
 
 
-filmsCopy.sort()
+/*people y su descripciones (yo)
 
+locaciones y descripciones (Alisson)
 
-reviewMovieScore
-alphabeticOrder
-
-// export const anotherExample = () => {
-//   console.log('OMG');
-// };
+vehiculos y susdescripciones (yo)*/
--- src/main.js
+++ src/main.js
@@ -1,44 +1,47 @@
-// import { setMoviesTitle } from './data.js'
-// import data from './data/ghibli/ghibli.js'
-import { setMoviesTitle } from './data.js';
+import data from './data/ghibli/ghibli.js'
 
-/*data.films.forEach(function (elem) {
-    console.log(elem.poster)
-});*/
+import { setMoviesTitle, alphabeticOrder, alphabeticOrderLess, ratingScore} from "./data.js";
 
-const navMovies = document.querySelector("#movies");
+const navMovies = document.querySelector("#pelis");
+const movieList = document.querySelector("#movieList");
+const filmsSelector = document.querySelector("#filmsFilter");
+
+function renderMovies(content) {
+   // movieList.innerHTML = "";
+    movieList.innerHTML = content;
+}
+
+
 navMovies.addEventListener("click", function () {
-    const hideFirstSlide = document.querySelector("#container_to_display");
-    const logo = document.querySelector("#logo");
-    hideFirstSlide.style.display = "none";
-    logo.style.display = "none";
-    //mostrar la imagen y el titulo
-    setMoviesTitle() // Filtrado seccion Peliculas con img + titulo
-    // anotherExample()
+    const films = data.films;
+    //join es para eliminar la coma
+    const moviesListString = setMoviesTitle(films).join("");
+    //const filmsCopy = [...data.films];
+    renderMovies(moviesListString);
+})
 
-});
+filmsSelector.onchange = function (e) {
+    const selectedFilter = e.target.value;
+    if (selectedFilter === "alphabeticallyMoreAndLess") {
+        const sortAlphabetically = alphabeticOrder(data.films);
+        const moviesListString = setMoviesTitle(sortAlphabetically).join("");
+
+        renderMovies(moviesListString);
+    }
+    if (selectedFilter === "alphabeticallyLessAndMore") {
+        const sortAlphabeticallyLess = alphabeticOrderLess(data.films);
+        const moviesLessString = setMoviesTitle(sortAlphabeticallyLess).join("");
+
+        renderMovies(moviesLessString);
+    }
+    if (selectedFilter === "rtScoreOrder") {
+        const sortRatingScore = ratingScore(data.films);
+        const filmsRt = setMoviesTitle(sortRatingScore).join("");
+
+        renderMovies(filmsRt);
+    }
+}
 
 
 
-// function pruebaDos() {
-//     data.films.forEach(function (element) {
-//         const movieCover = `<img src="${element.poster}" alt = "portada de pelicula" loading="lazy"/> `,
-//             moviesTitle = `<h3> ${element.title.toUpperCase()} </h3>`
-//         document.querySelector("#name-pelis").innerHTML += movieCover + moviesTitle
-//     });
-// }
 
-
-// function prueba() {
-//     data.films.forEach(function (elem) {
-//         const titulos = `<h2> ${elem.title.toUpperCase()} </h2>`,
-//             descripciones = `<h3>Description:</h3><p>${elem.description}</p>`,
-//             directores = `<h3>Directors:</h3><p>${elem.director}</p>`,
-//             productores = `<h3>Productores:</h3><p>${elem.producer}</p>`,
-//             portada = `<img src="${elem.poster}" alt = "portada de pelicula" loading="lazy"/> `,
-//             fechaLanzamiento = `<h3> Release Date:</h3> <p>${elem.release_date}</p>`,
-//             puntajeAudiencia = `${elem.release_date} `;
-//         document.querySelector("#name-pelis").innerHTML += titulos + descripciones + directores + productores + fechaLanzamiento + portada;
-
-//     });
-// }
