acaaadd4e51c11d04b1d41838cacc7b590f91245
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-07-22 13:45:09 -0500

    test de prueba

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,43 @@
-import { setMoviesTitle, alphabeticOrder} from '../src/data.js';
+import { alphabeticOrder } from '../src/data.js';
 
-
-describe('setMoviesTitle', () => {
-  it('is a function', () => {
-    expect(typeof setMoviesTitle).toBe('function');
-  });
-
-  it('returns `setMoviesTitle`', () => {
-    expect(setMoviesTitle()).toBe('setMoviesTitle');
-  });
-});
-
-
-describe('alphabeticOrder', () => {
+describe('ordenar por filtros', () => {
   it('is a function', () => {
     expect(typeof alphabeticOrder).toBe('function');
   });
 
-  it('returns `alphabeticOrder`', () => {
-    expect(alphabeticOrder()).toBe('alphabeticOrder');
+  it('Ordena desde a z', () => {
+    const data = [{
+      "title": "Castle in the Sky",
+      "rt_score": "95",
+      "release_date": "1986",
+    },
+    {
+      "title": "My Neighbor Totoro",
+      "rt_score": "93",
+      "release_date": "1988",
+    }];
+    const result = ["Castle in the Sky", "My Neighbor Totoro"];
+    expect(alphabeticOrder(data.title)).toEqual(result);
   });
 });
+
+// describe('alphabeticOrder', () => {
+// //   it('is a function', () => {
+// //     expect(typeof setMoviesTitle).toBe('function');
+// //   });
+
+// //   it('returns `setMoviesTitle`', () => {
+// //     expect(setMoviesTitle()).toBe('setMoviesTitle');
+// //   });
+// // });
+
+
+// // describe('alphabeticOrder', () => {
+// //   it('is a function', () => {
+// //     expect(typeof alphabeticOrder).toBe('function');
+// //   });
+
+// //   it('returns `alphabeticOrder`', () => {
+// //     expect(alphabeticOrder()).toBe('alphabeticOrder');
+// //   });
+// // });
